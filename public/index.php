83ed6297cfe746154d74f093b6b7f84b3a22dffa
liubang <it.liubang@gmail.com>
2018-10-30 16:43:48 +0800

    修复错误的用法

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-error_reporting(E_ALL | E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 define('APP_PATH', realpath(__DIR__ . '/../') . '/');
